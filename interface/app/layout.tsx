61211b4822a5bbf49decad1323364b8edef56083
abde1khaliq <abdelkhaliqqq@gmail.com>
2026-08-21 17:07:54 +0300

    feat: update metadata for improved SEO and social sharing

--- interface/app/layout.tsx
+++ interface/app/layout.tsx
@@ -9,8 +9,39 @@ const geistSans = Geist({
 });
 
 export const metadata: Metadata = {
-  title: "Korasa",
-  description: "Organize your studies today using Korasa.",
+  title: "Korasa | Study in Your Way",
+  description:
+    "Organize your studies all in one place today using Korasa.",
+
+  authors: [{ name: "Ahmed Abdelkhaliq" }],
+
+  openGraph: {
+    title: "Korasa | Study in Your Way",
+    description:
+      "Organize your studies all in one place today using Korasa.",
+    url: "https://korasa.study",
+    siteName: "Korasa",
+    images: [
+      {
+        url: "https://ik.imagekit.io/cin2tn3bj/korasa_logo.png?updatedAt=1787320657608",
+        width: 512,
+        height: 512,
+        alt: "Korasa - Study in Your Way",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Korasa | Study in Your Way",
+    description:
+      "Organize your studies all in one place today using Korasa.",
+    images: [
+      "https://ik.imagekit.io/cin2tn3bj/korasa_logo.png?updatedAt=1787320657608",
+    ],
+  },
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
