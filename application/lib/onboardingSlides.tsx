b2a4eb230ac394683ec4804d0c904a53a0cc0217
abde1khaliq <abdelkhaliqqq@gmail.com>
2026-08-29 21:58:39 +0300

    fix: correct formatting and indentation in onboardingSlides.tsx

--- application/lib/onboardingSlides.tsx
+++ application/lib/onboardingSlides.tsx
@@ -1,32 +1,37 @@
 export interface OnboardingSlide {
-  key: string;
-  title: string;
-  description: string;
+  key: string;
+  title: string;
+  description: string;
 }
 
 export const onboardingSlides: OnboardingSlide[] = [
-  {
-    key: "welcome",
-    title: "Create a Subject.",
-    description:
-      "Adding subjects like English, Math, Physics, Chemistry.",
-  },
-  {
-    key: "organize",
-    title: "Organize with folders",
-    description:
-      "Create folders within each subject to keep your questions perfectly categorized.",
-  },
-  {
-    key: "capture",
-    title: "Snap a question, add the answer",
-    description:
-      "Take a photo of a question, add it's answer, difficulty, a small note to remember e.g. the steps you took to solve it.",
-  },
-  {
-    key: "filter",
-    title: "You're all set!",
-    description:
-      "Add, filter, search, and review your questions anytime. Start discovering Korasa's features!",
-  },
+  {
+    key: "welcome",
+    title: "Create a Subject.",
+    description: "Adding subjects like English, Math, Physics, Chemistry.",
+  },
+  {
+    key: "organize",
+    title: "Organize with folders",
+    description:
+      "Create folders within each subject to keep your questions perfectly categorized.",
+  },
+  {
+    key: "capture",
+    title: "Snap a question, add the answer",
+    description:
+      "Take a photo of a question, add it's answer, difficulty, a small note to remember e.g. the steps you took to solve it.",
+  },
+  {
+    key: "test",
+    title: "Test yourself, by creating exams.",
+    description:
+      "Create an Exam, select which subject/folder you want to create the exam for, select the exam type, number of questions, difficulty and GO.",
+  },
+  {
+    key: "filter",
+    title: "You're all set!",
+    description:
+      "Add, filter, search, review your questions and test yourself anytime. Start discovering Korasa's features!",
+  },
 ];
